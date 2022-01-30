b79991212d591e727ec2d223413696d84c7f4bc0
zero-fighter <rauaha@gmail.com>
2022-01-30 13:39:32 +0600

    day 10 php 02 updated

--- DAY_10_PHP_02/app/classes/Example.php
+++ DAY_10_PHP_02/app/classes/Example.php
@@ -12,6 +12,7 @@ class Example
     public $x;
     public $y;
     public $z;
+    public $data = []; // Array
 
     // method
     public function index()
@@ -99,5 +100,78 @@ class Example
 //        echo '<br/>....<br/>';
 //        echo ($this->x < $this->y) || ($this->y < $this->z); // 1 && 1
 //        echo '<br/>....<br/>';
+
+//        ====== Statement =======
+//        * single line statement
+//        * conditional statement: IF, IF ELSE, IF ELSE IF, SWITCH
+//        * repeated statement: FOR, WHILE, DO WHILE, FOREACH
+
+//        $this->x = 10;
+//        if($this->x > 20) {
+//            echo $this->x; //blank
+//        }else {
+//            echo 'Hello BASIS';
+//        }
+
+//        $this->x = 100;
+//        if($this->x > 200) {
+//            echo 'Hello World';
+//        }
+//        elseif($this->x < 20)
+//        {
+//            echo $this->x;
+//        }
+//        else {
+//            echo 'Hello BASIS';
+//        }
+
+//        $this->x = 100;
+//        switch ($this->x)
+//        {
+//            case 10:
+//                echo 'Hello World';
+//                break;
+//            case 20:
+//                echo 'Hello BASIS';
+//                break;
+//            case 30:
+//                echo 'Hello Bangladesh';
+//                break;
+//            default:
+//                echo 'Hello Dhaka';
+//        }
+
+
+//        for($this->y = 10; $this->y <= 15; $this->y++)
+//        {
+//            echo 'Hello World<br/>';
+//        }
+//
+//        for($this->y = 99; $this->y >= 76; $this->y--)
+//        {
+//            echo $this->y.' ';
+//        }
+//
+//        $this->x = 100;
+//        while ($this->x < 110)
+//        {
+//            echo 'Hello World<br/>';
+//            $this->x++;
+//        }
+//
+//        $this->x = 100;
+//        do {
+//            echo 'Hello World<br/>';
+//            $this->x++;
+//        } while($this->x > 110);
+
+//        Array: a special variable who can hold multiple data, loosely coupled type
+        $this->data = [10, 20, 30, 'BASIS', 100.201, true];
+//        echo $this->data;
+//        echo $this->data[9];
+        foreach ($this->data as $item)
+        {
+            echo $item.' ';
+        }
     }
 }
