2b7a113930055d4527ae766a26c388f8d923b949
zero-fighter <rauaha@gmail.com>
2022-01-30 11:25:42 +0600

    day 10 before break

--- DAY_10_PHP_02/app/classes/Example.php
+++ DAY_10_PHP_02/app/classes/Example.php
@@ -3,12 +3,101 @@ namespace App\classes;
 
 class Example
 {
+    // property
     public $name;
     public $email;
     public $mobile;
+    public $firstName;
+    public $lastName;
+    public $x;
+    public $y;
+    public $z;
 
+    // method
     public function index()
     {
-        echo 'Hello World';
+//        datatype:
+//        echo 'Hello BASIS';
+//        $x = 10;
+//        $x = "10";
+//        $x = 10.132;
+//        $x = true;
+//        echo gettype($x);
+
+//        variable:
+//        $this->firstName = 'Ibn';
+//        $this->lastName = 'Fulan';
+//        echo "Name: ".$firstName." ".$lastName;
+
+//        operator:
+//        * Arithmetic operator
+//              * Binary (+, -, *, /, %)
+//              * Unary (++, --, (-))
+//        * Assignment operator (=, +=, -=, *=, /=, %=, .=)
+//        * Conditional operator (>, >=, <, <=, ==, !=, ===,!==)
+//        * Logical operator (&&, ||, !)
+
+        $this->x = 10;
+        $this->y = 20;
+        $this->z = 30;
+//        echo $this->x + $this->y;
+//        echo '<br/>';
+//        echo $this->x - $this->y;
+//        echo '<br/>';
+//        echo $this->x * $this->y;
+//        echo '<br/>';
+//        echo $this->x / $this->y;
+//        echo '<br/>';
+//        echo $this->x % $this->y;
+
+//        echo $this->x++; //10
+//        echo '<br/>';
+//        echo ++$this->x;  // 12
+//        echo '<br/>';
+//        echo $this->x--; // 12
+//        echo '<br/>';
+//        echo --$this->x; // 10
+//        echo '<br/>';
+//        echo -$this->x; // -10
+
+//        echo $this->x += $this->y; // x==30
+//        echo '<br/>';
+//        echo $this->x -= $this->y; // x==10
+//        echo '<br/>';
+//        echo $this->x *= $this->y; // x==200
+//        echo '<br/>';
+//        echo $this->x /= $this->y; // x==10
+//        echo '<br/>';
+//        echo $this->x %= $this->y; // x==10
+//        echo '<br/>';
+//        echo $this->x .= $this->y; // x==1020
+//        echo '<br/>';
+//        echo gettype($this->x);
+
+//        echo $this->x > $this->y; // blank means false
+//        echo $this->x < $this->y; // 1 means true
+//        echo $this->x == $this->y; // blank means false
+//        echo $this->x === $this->y; // blank means false, === checks strings also
+
+//        $this->x = 10;
+//        $this->y = 20;
+//        $this->z = 30;
+//        echo ($this->x > $this->y) && ($this->y > $this->z); // 0 && 0
+//        echo '<br/>....<br/>';
+//        echo ($this->x > $this->y) && ($this->y < $this->z); // 0 && 1
+//        echo '<br/>....<br/>';
+//        echo ($this->x < $this->y) && ($this->y > $this->z); // 1 && 0
+//        echo '<br/>....<br/>';
+//        echo ($this->x < $this->y) && ($this->y < $this->z); // 1 && 1
+//        echo '<br/>....<br/>';
+//
+//        echo ($this->x > $this->y) || ($this->y > $this->z); // 0 && 0
+//        echo '<br/>....<br/>';
+//        echo ($this->x > $this->y) || ($this->y < $this->z); // 0 && 1
+//        echo '<br/>....<br/>';
+//        echo ($this->x < $this->y) || ($this->y > $this->z); // 1 && 0
+//        echo '<br/>....<br/>';
+//        echo ($this->x < $this->y) || ($this->y < $this->z); // 1 && 1
+//        echo '<br/>....<br/>';
     }
 }
