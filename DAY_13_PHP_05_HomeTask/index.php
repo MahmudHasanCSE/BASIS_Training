1ee61812750e6d45f5445eb5bc010ce4e9dff8fa
zero-fighter <rauaha@gmail.com>
2022-02-02 22:40:49 +0600

    day 13 php 05 hometask added

---
+++ DAY_13_PHP_05_HomeTask/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'vendor/autoload.php';
+//use  App\classes\StringWordCount;
+
+//$series = new Series();
+//$series ->index();
+
+//$count = new StringWordCount();
+//$count ->index();
---
+++ DAY_13_PHP_05_HomeTask/pages/action.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once '../vendor/autoload.php';
+use App\classes\StringWordCount;
+
+if (isset($_POST['btn'])){
+    $count  = new StringWordCount($_POST);
+//    $result = $count->index();
+    $word   = $count -> wordCount();
+    $character   = $count -> characterCount();
+    include 'index.php';
+}else{
+    header('Location: index.php');
+}
+
