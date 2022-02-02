891f4a4a51902545d5a77fff6dfbfa4c86fd2878
sudipto-saha84 <sudiptosaha.diu84@gmail.com>
2022-02-02 13:06:03 +0600

    day 13 php 05 updated

--- DAY_13_PHP_05/app/classes/Series.php
+++ DAY_13_PHP_05/app/classes/Series.php
@@ -11,10 +11,17 @@ class Series
 
 //    protected $user;
 
+    protected $oddEven;
+
     public function __construct($post=null)
     {
         $this->startingNumber = $post['starting_number'];
         $this->endingNumber = $post['ending_number'];
+
+        if(isset($post['odd_even']))
+        {
+            $this->oddEven = $post['odd_even'];
+        }
     }
 
     public function index()
@@ -33,8 +40,43 @@ class Series
         }
         else
         {
-            return $this->smallToBigSeries();
+            if ($this->oddEven == 'odd')
+            {
+                return $this->smallToBigOddSeries();
+            }
+            else if ($this->oddEven == 'even')
+            {
+                return $this->smallToBigEvenSeries();
+            }
+            else
+            {
+                return $this->smallToBigSeries();
+            }
         }
+    }
+
+    protected function smallToBigOddSeries()
+    {
+        for ($this->i = $this->startingNumber; $this->i <= $this->endingNumber; $this->i++)
+        {
+            if($this->i % 2 != 0)
+            {
+                $this->result .= $this->i.' ';
+            }
+        }
+        return $this->result;
+    }
+
+    protected function smallToBigEvenSeries()
+    {
+        for ($this->i = $this->startingNumber; $this->i <= $this->endingNumber; $this->i++)
+        {
+            if($this->i % 2 == 0)
+            {
+                $this->result .= $this->i.' ';
+            }
+        }
+        return $this->result;
     }
 
     protected function smallToBigSeries()
